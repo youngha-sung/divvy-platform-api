08a501e191494f2083dfbef55956a32e0be03146
younghasung <youngha.sung@gmail.com>
2020-03-12 04:09:57 -0500

    Secure the api with access token

--- app.js
+++ app.js
@@ -2,16 +2,54 @@ import express from 'express';
 import bodyparser from 'body-parser';
 import * as z from 'zebras';
 import get from 'lodash/get';
+import jwt from 'jsonwebtoken';
+import { secret } from './configs/configs';
 import { getStationById, getAgeGroupsAtStationsToday, getLatestTripsEndedAtStationsToday } from './data/db';
+import * as constants from './common/constants';
 
 const app = express();
+const ProtectedRoutes = express.Router();
 const port = process.env.PORT || 3200;
+const data = z.readCSV('data/Divvy_Trips_2019_Q2');
+
+//set secret
+app.set('Secret', secret);
 
 // middleware
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
+app.use('/', ProtectedRoutes);
 
-const data = z.readCSV('data/Divvy_Trips_2019_Q2')
+// generate token whenever server starts up for time being...
+const payload = {
+  check:  true
+};
+const token = jwt.sign(payload, app.get('Secret'), {
+  expiresIn: 1440 // expires in 24 hours
+});
+
+ProtectedRoutes.use((req, res, next) =>{
+  const token = req.headers['access-token'];
+  const secret = app.get('Secret');
+
+  if (token) {
+    jwt.verify(token, secret, (error, decoded) =>{
+      if (error) {
+        return res.status(401).send({
+          errorMessage: constants.UNAUTHORIZED_ERROR_MESSAGE,
+        });
+      } else {
+        req.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    return res.status(401).send({
+      errorMessage: constants.UNAUTHORIZED_ERROR_MESSAGE,
+    });
+
+  }
+});
 
 /**
  * DEFINITIONS:
@@ -37,7 +75,7 @@ const data = z.readCSV('data/Divvy_Trips_2019_Q2')
  * Given a station id,
  * return the information for one station
  */
-app.get('/station/:id', async (req, res) => {
+ProtectedRoutes.get('/station/:id', async (req, res) => {
   const station_id = req.params.id;
   const response = await getStationById(station_id);
 
@@ -52,7 +90,7 @@ app.get('/station/:id', async (req, res) => {
  * [0-20,21-30,31-40,41-50,51+, unknown],
  * who ended their trip at that station for a given day.
  */
-app.get('/age_groups', (req, res) => {
+ProtectedRoutes.get('/age_groups', (req, res) => {
   const endStations = get(req, 'query["stations"]', '').split(',');
   const response = getAgeGroupsAtStationsToday(endStations, data);
 
@@ -65,7 +103,7 @@ app.get('/age_groups', (req, res) => {
  * Given one or more stations,
  * return the last 20 trips that ended at each station for a single day.
  */
-app.get('/trips_to_stations', (req, res) => {
+ProtectedRoutes.get('/trips_to_stations', (req, res) => {
   const endStations = get(req, 'query["stations"]', '').split(',');
   const response = getLatestTripsEndedAtStationsToday(endStations, data);
 
@@ -74,4 +112,7 @@ app.get('/trips_to_stations', (req, res) => {
 
 app.listen(port, () => {
   console.log(`running at port ${port}`);
+  console.log('Generating Access Token');
+  console.log();
+  console.log(token);
 });
--- common/constants.js
+++ common/constants.js
@@ -6,3 +6,4 @@ export const LOCAL_END_TIME = '01 - Rental Details Local End Time';
 export const BIRTH_YEAR = '05 - Member Details Member Birthday Year';
 
 export const INTERNAL_SERVER_ERROR_MESSAGE = 'Internal Server Error';
+export const UNAUTHORIZED_ERROR_MESSAGE = 'Unauthorized';
